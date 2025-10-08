d716767ec7f67db9fff1a54883cbb00b9fc6b2f0
jzells09 <justinzeller2004@gmail.com>
2025-10-08 17:53:06 -0500

    Removed old folder

--- unplugged/src/server/database.js
+++
@@ -1,7 +0,0 @@
-const AWS = require("aws-sdk");
-
-AWS.config.update({ region: "us-east-1" }); // or your region
-
-const dynamoClient = new AWS.DynamoDB.DocumentClient();
-
-module.exports = dynamoClient;
