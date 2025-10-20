adf2ec488e7c9fd0861c31fccc41ac8227a4e22c
gwilson12 <gwilson113772@gmail.com>
2025-10-20 12:52:42 -0500

    Edited incorrect text on settings screen

--- src/client/screens/SettingsScreen.js
+++ src/client/screens/SettingsScreen.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 const SettingsScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Customize</Text>
+      <Text style={styles.title}>Settings</Text>
     </View>
   );
 };
