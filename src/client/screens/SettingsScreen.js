4f0fad05b8e07ce8b7154ed37942fbc8d741ab9f
gwilson12 <gwilson113772@gmail.com>
2025-10-20 12:52:03 -0500

    Added foundation for all screens

---
+++ src/client/screens/SettingsScreen.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SettingsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Customize</Text>
+    </View>
+  );
+};
+
+//temporary style sheet, replace with separate file or components later
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'flex-start',
+    backgroundColor: '#fff',
+  },
+  title: {
+    color: '#222E50',
+    fontFamily: 'Times New Roman',
+    fontSize: 48,
+    fontWeight: '600',
+    marginBottom: 32,
+    textAlign: 'left',
+  }
+});
+
+export default SettingsScreen;
