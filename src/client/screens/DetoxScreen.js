0f43540dda2e6178a4a70f50c34424a2ada669c0
gwilson12 <gwilson113772@gmail.com>
2025-11-07 15:16:24 -0600

    Added placeholders for detox screen

--- src/client/screens/DetoxScreen.js
+++ src/client/screens/DetoxScreen.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { ScrollView, Text, StyleSheet } from 'react-native';
+import { View, ScrollView, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const DetoxScreen = () => {
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.title}>Detox</Text>
+      <Text style={styles.description}>View stats and start working</Text>
+
+      <View style={styles.modesContainer}>
+        <Text style={styles.sectionTitle}>Start Mode</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Focus Mode</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Detox Mode</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.statsContainer}>
+        <Text style={styles.sectionTitle}>Detox Stats</Text>
+        <Text style={styles.body}>This is where you will see detox stats information!</Text>
+      </View>
     </ScrollView>
   );
 };
@@ -24,7 +40,28 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     marginBottom: 32,
     textAlign: 'left',
-  }
+  },
+  description: {
+
+  },
+  modesContainer: {
+
+  },
+  button: {
+
+  },
+  buttonText: {
+
+  },
+  statsContainer: {
+
+  },
+  sectionTitle: {
+
+  },
+  body: {
+
+  },
 });
 
 export default DetoxScreen;
