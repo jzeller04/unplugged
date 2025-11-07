953f1c105bded45cd4bb674fbee5e67f8924d673
gwilson12 <gwilson113772@gmail.com>
2025-11-07 14:43:38 -0600

    Added placeholders for customization screen

--- src/client/screens/CustomizeScreen.js
+++ src/client/screens/CustomizeScreen.js
@@ -1,30 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, StyleSheet, ScrollView } from 'react-native';
 
 const CustomizeScreen = () => {
+  const [detoxModes, setDetoxModes] = useState([]);
+  const [blockedLocations, setBlockedLocations] = useState([]);
+  const [scheduledDowntimes, setScheduledDowntimes] = useState([]);
+
   return (
     <ScrollView style={styles.container}>
-      <Text style={styles.title}>Customize</Text>
+      <Text style={styles.screenTitle}>Customize</Text>
+      <Text style={styles.description}>Set modes, apps blocked, and automatic blocking in certain hours or locations</Text>
+
+      <Section
+        title="Detox Modes"
+        items={detoxModes}
+        
+        //add functionality later
+        //addLabel="Add new mode"
+        //on press, navigate to new page to customize mode and add to array of modes
+        />
+
+      <Section
+        title="Blocked Locations"
+        items={blockedLocations}
+
+        //add functionality later
+        //addLabel="Add new location"
+        //on press, navigate to new page to customize location and add to array of locations
+        />
+
+      <Section
+        title="Scheduled Downtime"
+        items={scheduledDowntimes}
+
+        //add functionality later
+        //addLabel="Add new downtime"
+        //on press, navigate to new page to customize downtime and add to array of downtimes
+        />
     </ScrollView>
   );
 };
 
-//temporary style sheet, replace with separate file or components later
+//later add in button function to add new
+//also later add in functionality to edit button to navigate to edit page
+const Section = ({ title, items }) => (
+  <View style={styles.section}>
+    <Text style={styles.sectionTitle}>{title}</Text>
+    {items.map((item, index) => (
+      <View key={index} style={styles.itemRow}>
+        <Text>{item}</Text>
+        <Icon name="edit" size={20} />
+      </View>
+    ))}
+  </View>
+);
+
 const styles = StyleSheet.create({
-  container: {
+  screenContainer: {
     flex: 1,
     padding: 24,
     justifyContent: 'flex-start',
     backgroundColor: '#fff',
   },
-  title: {
+  screenTitle: {
     color: '#222E50',
     fontFamily: 'Times New Roman',
     fontSize: 48,
     fontWeight: '600',
     marginBottom: 32,
     textAlign: 'left',
-  }
+  },
+  description: {
+
+  },
+  section: {
+
+  },
+  sectionTitle: {
+
+  },
+  itemRow: {
+
+  },
 });
 
 export default CustomizeScreen;
